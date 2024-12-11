9136bacd8cf77ab2d5e32de10d7707a1d2fa4cd9
qurreee <114373469+qurreee@users.noreply.github.com>
2024-12-11 12:34:33 +0700

    clean

--- Project/Launcher.cpp
+++ Project/Launcher.cpp
@@ -1,33 +1,13 @@
 #include "Setting.h"
 #include "Game.h"
 
-#include "Lesson01_BgColor.h"
-#include "Lesson02_Lerp.h"
-#include "Lesson03_Animation.h"
-#include "Lesson04_Movement.h"
-#include "Lesson05_Input.h"
-#include "Lesson06_ObjectPool.h"
-#include "Lesson07_GUI.h"
-#include "Lesson08_Rotation.h"
-#include "Lesson09_Bullet.h"
-#include "Lesson10_Platform.h"
-#include "Lesson11_ParallaxScrolling.h"
-
-#include "LessonAI01_SteeringBehaviors.h"
-#include "LessonAI02_NPCWave.h"
-
-#include "Dino_Game.h"
-#include "DinoMain.h"
-
-
-#include "PPCoba.h"
 #include "PPManager.h"
 
 int main(int argc, char** argv) {
 	Engine::Setting* setting = new Engine::Setting();
-	setting->screenWidth = 1000;
+	setting->screenWidth = 1920; 
 	setting->screenHeight = 1080;
-	setting->windowFlag = Engine::WindowFlag::FULLSCREEN;
+	setting->windowFlag = Engine::WindowFlag::WINDOWED;
 	setting->vsync = true;
 	setting->targetFrameRate = 30;
 	Engine::Game* game = new Engine::PPManager(setting);
